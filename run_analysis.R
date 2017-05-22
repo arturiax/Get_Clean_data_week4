dd1552abc0e5ee97555d8ab5e4c991f79da12fc8
arturiax <arturiax@hotmail.com>
2017-05-22 14:55:59 +0000

    import and unzip data

---
+++ run_analysis.R
@@ -0,0 +1,11 @@
+
+library(readr)
+library(tidyr)
+library(dplyr)
+
+# We dowload the dataset from internet
+download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", "data.zip")
+unzip("data.zip")
+
+
+data <- read_csv()
