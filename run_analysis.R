c6e35c3ee24e95ae11784eee31adc6be6417fba2
arturiax <arturiax@hotmail.com>
2017-05-24 16:28:13 +0000

    almost there

--- run_analysis.R
+++ run_analysis.R
@@ -16,7 +16,7 @@ labels <- read.table("./UCI HAR Dataset/activity_labels.txt") # names of the act
 subtrain <- read.table("./UCI HAR Dataset/train/subject_train.txt") # subjects for the train records
 subtest <- read.table("./UCI HAR Dataset/test/subject_test.txt") #subject for the test records
 
-#The names of the features are not in a valid R form, we chage that
+#The names of the features are not in a valid R form, we change that
 valid_names <- make.names(features[,2], unique=TRUE)
 
 #We give names to the features on the train and test sets
@@ -48,28 +48,31 @@ final <- union(test, train)
 
 #We create a function to rename the features so the names are self-explanatory
 betternames <- function(name) {
-  
   type_of_signal <- ifelse(grepl("Body", name), "Body", "Gravity")
   type_of_tool <- ifelse(grepl("Acc", name), "_acc", "_gyro")
   jerk <- ifelse(grepl("Jerk", name), "_jerk", "_nojerk")
   magnitude <- ifelse(grepl("X", name), "_X", ifelse(grepl("Y", name), "_Y", ifelse(grepl("Z", name), "_Z", "_mag")))
   time_or_fourier <- ifelse(grepl("^t", name), "_t", "_f")
   stat <- ifelse(grepl("mean", name), "_mean", "_std")
+  
   paste0(type_of_signal, type_of_tool, jerk, magnitude, time_or_fourier, stat)
 }
 
 #We create the final tydydata set averaging by subject and activity the features that are
-# means or standar deviations of the signals
+# means or standard deviations of the signals
 
 tidydata <- final %>% 
   select(1:3, matches("\\.(mean|std)\\.")) %>% 
   group_by(set_type, subject, activity) %>% 
   summarise_all(mean)
 
+#We rename the variables
 colnames(tidydata)[4:69]<- betternames(colnames(tidydata)[4:69])
 
-write.table(tidydata, "final.txt", row.names = FALSE)
+#We write the dataset
+write.table(tidydata, "tidydata.txt", row.names = FALSE)
 
-##read.table("final.txt", header= T)
+#If you clone the repo you can read the table with:
+##read.table("tidydata.txt", header= T)
 
 
